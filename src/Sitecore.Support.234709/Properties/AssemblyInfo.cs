c5b2bacb452014d2b01d829c4ad0efe607414a8e
Ivan Sheyenko <ivs@sitecore.net>
2018-08-13 17:39:39 -0400

    add sc_lang query string to the form action URL

---
+++ src/Sitecore.Support.234709/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.234709")]
+[assembly: AssemblyProduct("Sitecore.Support.234709")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.234709/SetLanguage.cs
@@ -0,0 +1,20 @@
+﻿using Sitecore.ExperienceForms.Mvc.Pipelines.RenderForm;
+using Sitecore.Mvc.Pipelines;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.ExperienceForms.Mvc.Pipelines.RenderForm
+{
+  public class SetLanguage : MvcPipelineProcessor<RenderFormEventArgs>
+  {
+    public override void Process(RenderFormEventArgs args)
+    {
+      if (args.QueryString != null&& args.FormBuilderContext.FormBuilderMode==Sitecore.ExperienceForms.FormBuilderMode.Load)
+      {
+        args.QueryString["sc_lang"]= Sitecore.Context.Language.Name;
+      }
+    }
+  }
+}
